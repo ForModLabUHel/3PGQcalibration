d730d4c40cf1677afe29cca702c6995b87141c73
ForModLabUHel <francesco.minunno@helsinki.fi>
2023-08-24 17:41:41 +0100

    update

--- Rsrc/runModel.r
+++ Rsrc/runModel.r
@@ -1,3 +1,5 @@
+library(Metrics)
+library(gridExtra)
 library(r3PG)
 library(dplyr)
 library(ggplot2)
@@ -12,7 +14,10 @@ library(readxl)
 library(tictoc)
 
 ###read data
+setwd("C:/Users/minunno/Documents/github/3PGQcalibration")
 load('myData/dataInputs.rdata')
+load("myData/pMAP.rdata")
+
 parameters <- read_excel('myData/INPUT_cal.xlsx', sheet = 'd_parameters')
 pars_soilQlitter <- read_excel('myData/INPUT_cal.xlsx', sheet = 'd_parsQlitter')
 pars_SoilInit <- read_excel('myData/INPUT_cal.xlsx', sheet = 'pars_SoilInit')
@@ -21,15 +26,16 @@ climateData <- data.table(read.csv('myData/monthly_weather.csv'))
 ###remove 0 radiation
 climateData$srad[which(climateData$srad==0)]  <- 0.5
 par <- fread('myData/parameters.csv')
-
+npars <- length(par$best)
+pMAP <- pMAP[1:npars]
 source(url('https://raw.githubusercontent.com/ForModLabUHel/3PGQcalibration/master/Rsrc/functions.r'))
 
-if(exists("initFile")){
-  load(paste0("calOut/",initFile))
-  startValue <- rbind(MAP(calibration[[1]])$parametersMAP,
-                      MAP(calibration[[2]])$parametersMAP,
-                      MAP(calibration[[3]])$parametersMAP)
-}
+# if(exists("initFile")){
+#   load(paste0("calOut/",initFile))
+#   startValue <- rbind(MAP(calibration[[1]])$parametersMAP,
+#                       MAP(calibration[[2]])$parametersMAP,
+#                       MAP(calibration[[3]])$parametersMAP)
+# }
 
 sites <- siteData
 # 
@@ -37,78 +43,64 @@ sites <- siteData
 # sites <- sites[!sites %in% NAs]
 
 nSites <- length(sites)
-pErr <- rep(c(0.1,0.001),7)
+# pErr <- rep(c(0.1,0.001),7)
 # climate <- climate[1:1000,]
 
-## calibration settings
-if(!exists("calN")) calN <- 0
-if(!exists("iterations")) iterations=3e3
-if(!exists("nCores")) nCores = 1
-if(!exists("thin")) thin = 100
-if(!exists("nChains")) nChains <- 3
-
-print(thin)
-print(iterations)
-
-sets <-split(siteData, ceiling(seq_along(siteData)/100))
-
-###soilParamters
-pSoil <- c(7,0.36,0.25,0.5,1.1,0.3) #"beta", "eta", "e0", "fc", "q0", "z
-pErrSoil <- c(0,10) ##bias error, standard deviation
+# ## calibration settings
+# if(!exists("calN")) calN <- 0
+# if(!exists("iterations")) iterations=3e3
+# if(!exists("nCores")) nCores = 1
+# if(!exists("thin")) thin = 100
+# if(!exists("nChains")) nChains <- 3
+# 
+# print(thin)
+# print(iterations)
+# 
+# sets <-split(siteData, ceiling(seq_along(siteData)/100))
+# 
+# ###soilParamters
+# pSoil <- c(7,0.36,0.25,0.5,1.1,0.3) #"beta", "eta", "e0", "fc", "q0", "z
+# pErrSoil <- c(0,10) ##bias error, standard deviation
 
 # calibration
 ### Define the prior
 pIds <- c(1:7,10,13:15,31:32,39,41,44,47,49)
 noDecid_par <- 7
-# par <- list()
-# par$best2 <- c(parameters$`Pinus sylvestris`[pIds],parameters$`Picea abies`[pIds],
-#               parameters$`Pinus contorta`[pIds],parameters$`Betula alba`[pIds[-noDecid_par]],
-#               parameters$`other deciduous`[pIds[-noDecid_par]],pErr,pSoil,pErrSoil)
-# par$min <- par$best * 0.8
-# par$max <- par$best * 1.2
-# par$names <- c(paste0(parameters$parameter[pIds],"_pine"),
-#                paste0(parameters$parameter[pIds],"_spruce"),
-#                paste0(parameters$parameter[pIds],"_contorta"),
-#                paste0(parameters$parameter[pIds[-noDecid_par]],"_birch"),
-#                paste0(parameters$parameter[pIds[-noDecid_par]],"_decid"),
-#                "aN","bN","aB","bB","aV","bV","aD","bD",
-#                "aWs","bWs","aWr","bWr","aWf","bWf",
-#                "beta", "eta", "e0", "fc", "q0", "z",
-#                "biasSoil","sdSoil")
 
-# pY <- which(par$best<0)
-# par$min[pY] <- par$best[pY] * 1.2
-# par$max[pY] <- par$best[pY] * 0.8
-# pY <- which(par$best==0)
-# par$min[pY] <- par$best[pY] -0.1
-# par$max[pY] <- par$best[pY] + 0.1 
-# par$min[121] <- -10
-# par$max[121] <- 10
+dataOr <- runModelOut(par$best,sites)
+dataMAP <- runModelOut(pMAP,sites)
 
-if(!exists("startValue")){
-  prior <- createUniformPrior(lower = par$min, upper = par$max)
-  ### Create Bayesian Setup
-  BCmod <- createBayesianSetup(likelihood, prior, best = par$best,
-                               names = par$names)
+for(i in 1:length(dataMAP)) dataMAP[[i]]$siteID <- sites[i]
+for(i in 1:length(dataOr)) dataOr[[i]]$siteID <- sites[i]
+
+dataMAP <- do.call(rbind,dataMAP)
+dataOr <- do.call(rbind,dataOr)
+
+dataMAP$sims <- as.numeric(dataMAP$sims)
+dataMAP$obs <- as.numeric(dataMAP$obs)
+
+
+vars <- unique(dataMAP$var_name)
+vars <- vars[-4]
+plotListMAP <- plotListOr <- list()
+for(i in vars){
+  plotListMAP[[i]] <-  ggplot(data=dataMAP[var_name==i], aes(x=sims, y=obs)) +
+    geom_point() + geom_abline(slope=1,intercept=0)+
+    ggtitle(i)
   
-  startValue <- rbind(runif(length(par$best),par$min,par$max),
-                      runif(length(par$best),par$min,par$max),
-                      par$best)
-  
-  settings <- list(iterations = iterations, nrChains = nChains,thin=thin,startValue=startValue,
-                   message=FALSE,consoleUpdates=1000)
-  
-  tic(paste0("calibration time."," iteratios: ",iterations))
-  calibration <- runMCMC(BCmod, sampler="DEzs", settings = settings)
-  toc()
-}else{
-  if(exists("fx")){
-    calibration[[1]]$settings$f <- calibration[[2]]$settings$f <- 
-      calibration[[3]]$settings$f <- fx
-  } 
-  tic(paste0("calibration time."," iteratios: ",iterations))
-  calibration = runMCMC(calibration)
-  toc()
+  plotListOr[[i]] <- ggplot(data=dataOr[var_name==i], aes(x=sims, y=obs)) +
+    geom_point() + geom_abline(slope=1,intercept=0) +
+    ggtitle(i)
 }
 
-save(calibration, file=paste0("calOut/calibration_",calN,".rdata"))
+pdf("resultsCal.pdf")
+grid.arrange(grobs = c(plotListOr, plotListMAP), ncol = 2, as.table = FALSE)
+dev.off()
+
+rmseTab <- matrix(NA, length(vars),2)
+for(i in 1:length(vars)){
+  rmseTab[i,1] <- rmse(dataOr[var_name==vars[i]]$obs, dataOr[var_name==vars[i]]$sims)
+  rmseTab[i,2] <- rmse(dataMAP[var_name==vars[i]]$obs, dataMAP[var_name==vars[i]]$sims)
+}
+colnames(rmseTab) <- c("original","calibrated")
+rownames(rmseTab) <- vars
